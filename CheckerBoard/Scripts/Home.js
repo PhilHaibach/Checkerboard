3a014a2df2756c104e457bd60e0add7b577958b8
PhilHaibach <phaibach@yahoo.com>
2017-10-16 20:12:30 -0400

    Add project files.

---
+++ CheckerBoard/Scripts/Home.js
@@ -0,0 +1,48 @@
+﻿var isHoldingPiece = false;
+var pieceInHand = {};
+
+$(document).ready(function ()
+{
+    var cells = $(".cell");
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++)
+    {
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 == 0;
+        var isNextRow = (i + 1) % 8 == 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "navy" : "white");
+    }
+
+    $(".cell").click(function () {
+        
+        if (!isHoldingPiece) {
+            if ($(this).hasClass("piece")) {
+                PickUpPiece(this)
+            }
+        }
+        else {
+            DropPiece(this)
+        }
+    });
+});
+
+PickUpPiece = function (element) {
+
+    $(element).css("border", "1px solid red");
+
+    pieceInHand = element;
+    isHoldingPiece = true;
+}
+
+DropPiece = function (targetElement) {
+
+    targetElement.className = pieceInHand.className;
+
+    $(pieceInHand).css("border", "1px solid black");
+    pieceInHand.className = "cell";
+
+    isHoldingPiece = false;
+    pieceInHand = {};
+}
